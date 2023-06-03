4576b7bdb4a9e637f93720fb221d9567510ebc49
Daniel Newton <kdaniel.newton@gmail.com>
2023-06-02 21:22:14 -0400

    Add card images

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,10 @@
 export default function Card({ card, disabled, onClick }: any) {
   return (
-    <div className={disabled ? 'disabled' : ''} onClick={onClick}>
-      {card.id}
-    </div>
+    <img
+      src={`/images/cards/${card.id}.png`}
+      alt={card.id}
+      className={disabled ? 'disabled' : ''}
+      onClick={disabled ? null : onClick}
+    />
   );
 }
